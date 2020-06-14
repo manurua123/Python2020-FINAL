9ad14442b336c4429c1ee76ff4830fbb80b8b8d4
manurua123 <62101533+manurua123@users.noreply.github.com>
2020-06-14 16:45:29 -0300

    actualizacion grafica configuracion
    se cambio los colores y tamaños de los botones

--- configuracion.py
+++ configuracion.py
@@ -1,6 +1,13 @@
 import random
 import PySimpleGUI as sg
+sg.SetOptions(background_color='#222831',
+       text_element_background_color='#222831',
+       element_background_color='#9FB8AD',
+       button_color=('#222831','#00adb5'),
+       text_justification='center',
+       border_width=1,
 
+       )
 letras_puntos={'a':1,'b':3,'c':2,'d':2,'e':1,'f':4,'g':2,'h':4,'i':1,'j':6,'k':8,'l':1,'m':3,'n':1,'o':1,'p':3,'q':8,'r':1,'s':1,'t':1,'u':1,'v':4,'w':8,'x':8,'y':4,'z':10}
 letras_cantidad={'a':11,'b':3,'c':4,'d':4,'e':11,'f':2,'g':2,'h':2,'i':6,'j':2,'k':1,'l':4,'m':3,'n':5,'o':8,'p':2,'q':1,'r':4,'s':7,'t':4,'u':6,'v':2,'w':2,'x':1,'y':1,'z':1}
 
@@ -15,27 +22,28 @@ def nivel_medio(letrasPuntos,letrasCantidad):
     for i in letrasPuntos.keys():
         letrasPuntos[i] = letrasPuntos[i] *2
         letrasCantidad[i] = letrasCantidad[i]*2
-    tipoPalabra = ['/NM','/VB']
+    tipoPalabra = ['/VB','/JJ']
     return {'PuntajeLetra':letrasPuntos,'CantidadLetras':letrasCantidad,'TipoPalabre':tipoPalabra}
 
 def nivel_dificil(letrasPuntos,letrasCantidad):
     for i in letrasPuntos.keys():
         letrasPuntos[i] = letrasPuntos[i]
         letrasCantidad[i] = letrasCantidad[i]
-    tipos= ['/NM','/VB','/JJ']
+    tipos= ['/VB','/JJ']
     tipoPalabra=list(random.choice(tipos))
     return {'PuntajeLetra':letrasPuntos,'CantidadLetras':letrasCantidad,'TipoPalabre':tipoPalabra}
 
 layout = [
-    [sg.Text('Duracion del turno'), sg.Slider(range = (10, 120),orientation = 'h', size = (30,20), default_value = 60, key = 'tiempo',tooltip='Duracion de cad turno. 60seg por defecto')],
+    [sg.Text('Duracion del turno'), sg.Slider(background_color='#222831',range = (10, 120),orientation = 'h', size = (20,20), default_value = 60, key = 'tiempo',tooltip='Duracion de cad turno. 60seg por defecto')],
     [sg.Text('Nivel de dificultad'),
-    sg.Button('Facil',tooltip='Fichas: Muchas\nPuntos: Muchos\nCategorias: Sustantivos | Adjetivos | Verbos',auto_size_button=False),
-    sg.Button('Medio',tooltip='Fichas: Algunas\nPuntos: Normal\nCategorias: Sustantivos | Adjetivos ',auto_size_button=False),
-    sg.Button('Dificil',tooltip='Fichas: Pocas\nPuntos: Pocos\nCategorias:Aleatoria',auto_size_button=False)],
-    [sg.Button('Confirmar',disabled=True),sg.Button('Cancelar')],
+    sg.Button('Facil',tooltip='Fichas: Muchas\nPuntos: Muchos\nCategorias: TODAS',size= (8,1),pad=(2,4)),
+    sg.Button('Medio',tooltip='Fichas: Algunas\nPuntos: Normal\nCategorias: Verbos | Adjetivos ',size= (8,1),pad=(2,4)),
+    sg.Button('Dificil',tooltip='Fichas: Pocas\nPuntos: Pocos\nCategorias:Verbos | Adjetivos',size= (8,1),pad=(2,4))],
+    [sg.Button('Confirmar',disabled=True,size= (200,1),pad=(1,4))],
+    [sg.Button('Cancelar',size= (200,1),pad=(1,4))],
 ]
 def main():
-    window = sg.Window('', layout)
+    window = sg.Window('Configuracion', layout, text_justification='center',size= (400,180),font=("Helvetica", 13))
     while True:
         event, value = window.read()
         if event is None or event == 'Cancelar':
@@ -63,6 +71,7 @@ def main():
         if event == 'Confirmar':
             listaConfiguracion['Tiempo'] = value['tiempo']
             break
+    print(listaConfiguracion)
     window.close()
     return listaConfiguracion
 
